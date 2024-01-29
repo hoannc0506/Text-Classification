19c466cdf4d6c7d24e76a462d64bec4bd76e0175
hoannc0506 <hoan.nc0506@gmail.com>
2024-01-29 13:48:53 +0700

    Update vocab_builders

--- utils/vocab_builders.py
+++ utils/vocab_builders.py
@@ -1,6 +1,8 @@
 from torchtext.vocab import build_vocab_from_iterator, Vocab
 from torchtext.data.utils import get_tokenizer
-
+from tokenizer import Tokenizer
+from tokenizer.models import BPE
+from tokenizer.pre_tokenizers import Whitespace
 
 def yield_tokens(data_iter, tokenizer):
     for data in data_iter:
@@ -15,6 +17,13 @@ def build_basic_vocab(data, min_freq, vocab_size):
     vocab.set_default_index(vocab["<unk>"])
     
     return vocab, tokenizer
+
+
+def build_bpe_vocab(data, min_freq, vocab_size):
+    bpe_tokenizer = Tokenizer(BPE())
+    bpe_tokenizer.pre_tokenizer = Whitespace()
+    
+    tokenized_data = 
 
 
 
