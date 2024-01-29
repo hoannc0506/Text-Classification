2aa8486f68c895a7137432b3f208a096ce788e2b
hoannc0506 <hoan.nc0506@gmail.com>
2024-01-29 14:32:44 +0700

    Update BPE tokenizer

--- utils/vocab_builders.py
+++ utils/vocab_builders.py
@@ -1,12 +1,17 @@
-from torchtext.vocab import build_vocab_from_iterator, Vocab
+from torchtext.vocab import build_vocab_from_iterator, vocab, Vocab
 from torchtext.data.utils import get_tokenizer
-from tokenizer import Tokenizer
-from tokenizer.models import BPE
-from tokenizer.pre_tokenizers import Whitespace
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
 
 def yield_tokens(data_iter, tokenizer):
     for data in data_iter:
         yield tokenizer(data['text'])
+
+def imdb_data_iter(data):
+    for sample in data:
+        yield sample['text']
 
 def build_basic_vocab(data, min_freq, vocab_size):
     tokenizer = get_tokenizer("basic_english")
@@ -22,9 +27,33 @@ def build_basic_vocab(data, min_freq, vocab_size):
 def build_bpe_vocab(data, min_freq, vocab_size):
     bpe_tokenizer = Tokenizer(BPE())
     bpe_tokenizer.pre_tokenizer = Whitespace()
+    trainer = BpeTrainer(vocab_size=vocab_size,
+                         min_frequency=min_freq,
+                         special_tokens=["<pad>", "<s>", "<unk>"])
     
-    tokenized_data = 
+    bpe_tokenizer.train_from_iterator(iterator=imdb_data_iter(data),
+                                      trainer=trainer,
+                                      length=len(data))
+    
+    bpe_vocab = vocab(ordered_dict=bpe_tokenizer.get_vocab(),
+                      min_freq=min_freq, 
+                      specials=["<pad>", "<s>", "<unk>"])
 
+    bpe_vocab.set_default_index(bpe_vocab["<unk>"])
+    
+    return bpe_vocab, bpe_tokenizer
 
+if __name__ == "__main__":
+    from datasets import load_dataset
+    imdb = load_dataset("imdb")
+    train_data, test_data = imdb['train'], imdb['test']
+    
+    test_vocab, test_tokenizer = build_bpe_vocab(train_data, min_freq=3, vocab_size=20000)
+    
+    import pdb; pdb.set_trace()
+    print('hear')
+
+    
+    
 
 
